9aee173397c9b111c65ffc1deba0bc04ac51e297
ViralSir <viralsir2018@gmail.com>
2021-10-10 10:58:17 +0530

    changed the no variables

--- exceptiondemo.py
+++ exceptiondemo.py
@@ -1,11 +1,11 @@
 start=True
 while start==True:
     try:
-        no1=int(input("Enter No1:"))
-        no2=int(input("Enter No2:"))
+        number1=int(input("Enter No1:"))
+        number2=int(input("Enter No2:"))
 
-        ans=no1/no2
-        print("ans:",no1/no2)
+        ans=number1/number2
+        print("ans:",number1/number2)
         start=False
     except ZeroDivisionError:
          print("zero is invalid as a second no")
